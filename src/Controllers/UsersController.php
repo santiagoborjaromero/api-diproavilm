7120c7403a062ccb40ac48f3801886af622f998b
Santiago Borja Romero <santiago@callevo.com>
2024-07-24 11:28:26 -0500

    se quita el campo password en la entrega de datos

--- src/Controllers/UsersController.php
+++ src/Controllers/UsersController.php
@@ -13,7 +13,7 @@ class UsersController extends Controller{
 
         $conn = new ConnController();
         $conn->Connect("mysql");
-        $rec = $conn->Execute("SELECT * FROM user WHERE username = :username AND password=:password AND deleted_at IS NULL LIMIT 1", $requestBody);
+        $rec = $conn->Execute("SELECT * FROM user WHERE username=:username AND password=:password AND status=1 AND deleted_at IS NULL LIMIT 1", $requestBody);
 
         $status = "ok";
         $message = $rec ;
@@ -23,39 +23,41 @@ class UsersController extends Controller{
 
         if (!$rec){
             $status = "error";
-            $message = "Usuario o contraseña no existen o se encuentran suspendidos";
+            $message = "Usuario invalido o inactivo";
         } else {
-            $iduser = $rec[0]["iduser"];
+            unset($message[0]["password"]); // No se puede enviar esta informacion es sencible
 
-            $idrole = $rec[0]["idrole"];
+            $iduser = $message[0]["iduser"];
+
+            $idrole = $message[0]["idrole"];
             $recRolMenu = $conn->Execute("SELECT * FROM view_get_menu WHERE idrole = :idrole", ["idrole"=>$idrole]);
 
-            $idrole = $rec[0]["idrole"];
+            $idrole = $message[0]["idrole"];
             $recRol = $conn->Execute("SELECT * FROM role WHERE idrole = :idrole", ["idrole"=>$idrole]);
-        }
-        
-        $message[0]["menu"] = $recRolMenu;
-        $message[0]["role"] = $recRol;
-        
-        // print($message[0]["token"]);
-
-        if ($message[0]["token"]===null){
-            $token = UsersController::setToken( $message[0] );
-            // print($token);
-            $message[0]["token"] = $token;
-        } else{
-            $token = $message[0]["token"];
-        }
-        
-        $verificacion = UsersController::validToken($token, $message[0]);
-        if(!$verificacion){
-            $status = "error";
-            $message = "Token ha expirado";
-        } else {
-            $sql = "UPDATE user 
-                    SET token = :token, lastlogged = :lastlogged
-                    WHERE iduser= :iduser";
-            $recRol = $conn->Execute($sql, ["iduser"=>$iduser,"token"=>$token, "lastlogged" => date('Y-m-d H:i:s')]);
+            
+            $message[0]["menu"] = $recRolMenu;
+            $message[0]["role"] = $recRol;
+            
+            // print($message[0]["token"]);
+    
+            if ($message[0]["token"]===null){
+                $token = UsersController::setToken( $message[0] );
+                // print($token);
+                $message[0]["token"] = $token;
+            } else{
+                $token = $message[0]["token"];
+            }
+            
+            $verificacion = UsersController::validToken($token, $message[0]);
+            if($verificacion != 'ok'){
+                $status = "error";
+                $message = $verificacion;
+            } else {
+                $sql = "UPDATE user 
+                        SET token = :token, lastlogged = :lastlogged
+                        WHERE iduser= :iduser";
+                $recRol = $conn->Execute($sql, ["iduser"=>$iduser,"token"=>$token, "lastlogged" => date('Y-m-d H:i:s')]);
+            }
         }
 
         http_response_code(200);
@@ -74,7 +76,7 @@ class UsersController extends Controller{
     static public function setToken($user){
         // date_default_timezone_set("America/Guayaquil");
         $payload = [
-            "user" => $user["iduser"],
+            "iduser" => $user["iduser"],
             "idrole" => $user["idrole"],
             "expire_at" => date('Y-m-d H:i:s', strtotime(date('Y-m-d H:i:s') . ' +1 day'))
         ];
@@ -89,18 +91,18 @@ class UsersController extends Controller{
         // date_default_timezone_set("America/Guayaquil");
 
         $payload = json_decode(Controller::decode(base64_decode($token)), true);
-
-        $auth = false;
+        $auth = "";
         if ($user["iduser"] == $payload["iduser"]){
-            $auth = true;
+            $auth = "ok";
 
-            if ( date($payload["expire_at"]) >= date("Y-m-d H:i:s")){
-                $auth = true;
+            if ( $payload["expire_at"] >= date("Y-m-d H:i:s")){
+                $auth = "ok";
             }else{
-                $auth = false;
+                $auth = "Token ha expirado";
             }
         }else{
-            $auth = false;
+            $auth = "Token no valido, se guardara su IP para auditoria";
+            //GUARDAR AUDITORIA
         }
         
         return $auth;
