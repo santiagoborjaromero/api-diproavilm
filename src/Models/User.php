d4c5c6d7cb74ea33898c4d489b5a3d9134caee00
Santiago Borja Romero <santiago.borja@gmail.com>
2024-08-01 10:38:30 -0500

    contenido de modelo aun no se utiliza, entonces se comenta

--- src/Models/User.php
+++ src/Models/User.php
@@ -3,14 +3,14 @@
 use Models;
 
 class User{
-    private $table = "user";
-    private $primary_key = "iduser";
-    public static $whereSQL = [];
+    // private $table = "user";
+    // private $primary_key = "iduser";
+    // public static $whereSQL = [];
     
-    static public function where($field, $connector, $value){
-        $whereSQL[] = [$field . " " . $connector . " " . $value];
-        var_dump($whereSQL);
-    }
+    // static public function where($field, $connector, $value){
+    //     $whereSQL[] = [$field . " " . $connector . " " . $value];
+    //     var_dump($whereSQL);
+    // }
 
     // static public function whereOr($field, $connector, $value){
     //     $this->whereSQL[] = [$field . " " . $connector . " " . $value];
