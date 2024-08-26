33b2645c9727c81e0d45496fe5393678b1612ba0
mrycky23 <rycky_jvm@outlook.com>
2024-08-26 00:19:26 -0500

    formulario config

--- src/Controllers/ConfigController.php
+++ src/Controllers/ConfigController.php
@@ -12,4 +12,52 @@ class ConfigController extends Controller{
         http_response_code(200);
         echo Controller::formatoSalida("ok",$rs);
     }
+    
+    static public function save(){
+        
+        Middleware::auditSecurity(); //valida el token
+
+        $requestBody = Middleware::request(); //recoge los datos
+
+        $config = new Model("config");
+        $config->where("variable","=",$requestBody["variable"]);
+        $rs = $config->get();
+
+        if ($rs != NULL){
+            $status = "error";
+            $message = "La configuracion ya existe, no se puede duplicar";
+        } else{
+            $config = new Model("config");
+            $d = $config->insertRecord($requestBody);
+            $status = "ok";
+            $message = $d;
+        }
+        http_response_code(200);
+        echo Controller::formatoSalida($status,$message);
+    }
+
+    static public function update(){
+        
+        Middleware::auditSecurity();
+        
+
+        $requestBody = Middleware::request(); //recoge los datos
+        $id= $_GET["id"];
+        $config = new Model("config");
+        $config->where("variable","=",$requestBody["variable"]);
+        $rs = $config->get();
+
+        if ($rs == NULL){
+            $status = "error";
+            $message = "La configuracion no existe";
+        } else{
+            $config = new Model("config");
+            $config->where("idconfig","=",$id);
+            $d = $config->updateRecord($requestBody);
+            $status = "ok";
+            $message = $d;
+        }
+        http_response_code(200);
+        echo Controller::formatoSalida($status,$message);
+    }
 }
