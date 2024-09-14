ead51bdac6851310e63abab218e1a2ff0b49035d
Santiago Borja Romero <santiago@callevo.com>
2024-09-14 00:30:21 -0500

    comentarios

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ spl_autoload_register(function ($class){
 
 set_exception_handler("ErrorHandler::handleException");
 
-// header("Access-Control-Allow-Origin: *");
+// header("Access-Control-Allow-Origin: *"); *//////
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST, GET, PUT, DELETE");
 header("Access-Control-Max-Age: 3600");
