0f4175a624c92856e617fd0d9f3916f005673c8c
Santiago Borja Romero <santiago.borja@gmail.com>
2025-03-01 02:48:20 -0500

    FEAT: Implementacion de inserts para la tabla “errorslog” que permita registrar errores provenientes de fallas en el api por base de datos, sintaxions sql, entre otros.

--- src/Controllers/ErrorHandler.php
+++ src/Controllers/ErrorHandler.php
@@ -1,17 +1,25 @@
 <?PHP
 
+require_once(__DIR__."/../Models/Model.php");
 //TODO: Metodo Para gestionar y estandarizar errores
+
 class ErrorHandler{
 
     public static function handleException(Throwable $exception): void {
+        $dataerror = [
+            "code" => $exception->getCode(),
+            "message" => $exception->getMessage(),
+            "file" => $exception->getFile(),
+            "line" => $exception->getLine(),
+            "trace" => json_encode($exception->getTrace()),
+        ];
+
+        $errs = new Model("errorslog");
+        $errs->insertRecord($dataerror);
+
         echo json_encode([
             "status" => "error",
-            "message" => [
-                "code" => $exception->getCode(),
-                "message" => $exception->getMessage(),
-                "file" => $exception->getFile(),
-                "line" => $exception->getLine()
-            ]
+            "message" => Controller::encode(json_encode($dataerror))
         ]);
     }
 
